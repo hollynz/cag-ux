22dea6f1ca212627c4e4c5ab3250328c28e2d1fa
Glitch (hello-webpage) <none>
2019-02-20 08:57:04 +0000

    🍂🌸 Checkpoint ./css/styles.css:3199100/46 ./index.html:3199100/6417 ./js/index.js:3199100/28

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 // let controlla1 = $('.sectionizr').sectionizr();
-let [controlla1, controlla2, controlla3] = $('.sectionizr').sectionizr();
+let [controlla1, controlla2, controlla3, controlla4, controlla5] = $('.sectionizr').sectionizr();
 
 
