9c7cdf8974e5f133326a571347d667d588e24eb1
Glitch (cag-insights-proj) <none>
2019-02-22 06:25:50 +0000

    👴💂 Checkpoint ./js/index.js:3199100/453

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
    $(function() {
       $('#slides-1, #slides-2').superslides({
-        hashchange: false
+        
       });
     });
    $(function() {
-      $('#slides-3', '#slides-4').superslides({
-        hashchange: false
+      $('#slides-3').superslides({
+        
       });
     });
+ $(function() {
+    $('#slides-4').superslides({
+        
+    });
+});
