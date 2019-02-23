b483018382646331625f443422fc53e430ff18f2
Glitch (cag-insights-proj) <none>
2019-02-23 04:37:52 +0000

    🦁👅 Checkpoint ./css/vendors/superslides.css:3199114/11 ./js/index.js:3199114/107 ./css/styles.css:3199114/36 ./.glitch-assets:3199114/913 ./index.html:3199114/485

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
    $(function() {
       $('#slides-1, #slides-2').superslides({
-        pagination: true
       });
     });
    $(function() {
       $('#slides-3').superslides({
-        
       });
     });
  $(function() {
     $('#slides-4').superslides({
-        
     });
 });
