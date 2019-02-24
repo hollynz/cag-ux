8d2de94de991e189ece2d1cbf912ab9cc036f671
Glitch (cag-insights-proj) <none>
2019-02-24 01:08:14 +0000

    🍈📺 Checkpoint ./index.html:3199100/165:3199114/191 ./js/index.js:3199100/264

--- js/index.js
+++ js/index.js
@@ -15,25 +15,21 @@ var ctx = document.getElementById("myChart1").getContext('2d');
 var myChart1 = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["<18", "18–25", "26–41", "41+"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Age',
+            data: [44, 28, 20, 8],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(75, 192, 192, 0.2)'
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(75, 192, 192, 1)'
             ],
             borderWidth: 1
         }]
@@ -42,7 +38,7 @@ var myChart1 = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero: true
                 }
             }]
         }
@@ -51,7 +47,7 @@ var myChart1 = new Chart(ctx, {
 
 var ctx2 = document.getElementById("myChart2").getContext('2d');
 var myChart2 = new Chart(ctx2, {
-    type: 'line',
+    type: 'bar',
     data: {
         labels: ["R", "B", "Y", "G", "P", "O"],
         datasets: [{
