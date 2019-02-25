c4c820e1143205b3e67cef5e0c8a63852a89b8ed
Glitch (cag-insights-proj) <none>
2019-02-25 02:55:01 +0000

    🐙🌿 Checkpoint ./js/index.js:3199100/1370

--- js/index.js
+++ js/index.js
@@ -26,10 +26,10 @@ var myChart1 = new Chart(ctx, {
                 '#67727a;'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
+                '#222222',
+                '#222222',
+                '#222222',
+                '#222222'
             ],
             borderWidth: 1
         }]
@@ -54,13 +54,12 @@ var myChart2 = new Chart(ctx2, {
             label: 'Used Site Before',
             data: [12, 88],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)'
+                '#67727a',
+                '#d75c37'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
+                '#222222',
+                '#222222'
             ],
             borderWidth: 1
         }]
@@ -85,14 +84,14 @@ var myChart3 = new Chart(ctx3, {
             label: 'Mobile vs Computer Usage',
             data: [52.17, 34.78, 13.04],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
+                '#d75c37',
+                '#67727a',
+                '#c3d7df'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
+                '#222222',
+                '#222222',
+                '#222222'
             ],
             borderWidth: 1
         }]
